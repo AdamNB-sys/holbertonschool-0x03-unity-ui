3e89cf29ef5a07ce1fb4863fabd65abe168f0090
AdamNB-sys <a1adnat@gmail.com>
2022-10-14 18:30:19 -0500

    WE ARE ACCESSIBLE

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,9 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
     // Loads maze scene when Play button is selected
     public void PlayMaze()
     {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
         SceneManager.LoadScene("maze");
     }
 
