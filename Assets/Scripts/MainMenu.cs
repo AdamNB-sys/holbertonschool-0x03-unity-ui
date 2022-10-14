ac11264225837c5296193ed4dae081c97fb94718
AdamNB-sys <a1adnat@gmail.com>
2022-10-14 17:35:26 -0500

    Quit application method added to Quit button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,9 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Update is called once per frame
+    // Loads maze scene when Play button is selected
     public void PlayMaze()
     {
         SceneManager.LoadScene("maze");
+    }
+
+    // Quits application when Quit button is selected
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
     }
 }
