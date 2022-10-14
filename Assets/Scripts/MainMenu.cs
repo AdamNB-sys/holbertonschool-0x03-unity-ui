b0c025d5f310ba86c06db140609718ce09cd33e1
AdamNB-sys <a1adnat@gmail.com>
2022-10-14 17:26:08 -0500

    added script for play button interaction

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Update is called once per frame
+    public void PlayMaze()
+    {
+        SceneManager.LoadScene("maze");
+    }
+}
